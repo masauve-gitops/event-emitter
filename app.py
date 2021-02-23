5b5e21f4c26d9995789cff0a49dd2d9109b7ebac
Martin Sauve <msauve@redhat.com>
2021-02-23 13:16:36 -0500

    change event

--- app.py
+++ app.py
@@ -9,17 +9,10 @@ import uuid
 from kafka import KafkaProducer
 
 EVENT_TEMPLATES = [
-    { "eventCategory": "CC_BALANCE_PAYMENT", "eventValue": "LATE_PAYMENT", "eventSource": "CUSTOMERCARE"},
-    { "eventCategory": "CC_BALANCE_PAYMENT", "eventValue": "MIN_DUE", "eventSource": "MOBLE"},
-    { "eventCategory": "CC_BALANCE_PAYMENT", "eventValue": "MIN_DUE", "eventSource": "WEBSITE"},
-    { "eventCategory": "CC_TRANSACTION", "eventValue": "AIRLINE_PURCHASE", "eventSource": "WEBSITE"},
-    { "eventCategory": "CC_TRANSACTION", "eventValue": "MERCHANT_PURCHASE", "eventSource": "POS"},
-    { "eventCategory": "CC_TRANSACTION", "eventValue": "HOTEL_PURCHASE", "eventSource": "POS"},
-    { "eventCategory": "CC_TRANSACTION", "eventValue": "ONLINE_PURCHASE", "eventSource": "WEBSITE"},
-    { "eventCategory": "DISPUTES", "eventValue": "CASE_CREATED", "eventSource": "IVR"},
-    { "eventCategory": "DISPUTES", "eventValue": "CASE_CLOSED", "eventSource": "IVR"},
-    { "eventCategory": "ONLINE_ACCOUNT", "eventValue": "PAYMENT_FAILURE", "eventSource": "CUSTOMERCARE"},
-    { "eventCategory": "ONLINE_ACCOUNT", "eventValue": "PAYMENT_SUCCESS", "eventSource": "CUSTOMERCARE"}
+    { "eventCategory": "STATION", "eventValue": "HIGH_VOLUME", "eventSource": "CAMERA"},
+    { "eventCategory": "STATION", "eventValue": "HIGH_VOLUME", "eventSource": "CAMERA"},
+    { "eventCategory": "TRAIN", "eventValue": "PARTS_WARNING", "eventSource": "SENSOR"},
+    { "eventCategory": "STATION", "eventValue": "HIGH_VOLUME", "eventSource": "CAMERA"}
 ]
 
 
@@ -32,8 +25,8 @@ ATM_EVENT = [
 
 CUSTOMER = [
 
-    'John',
-    'James'
+    'Berri UQAM',
+    'Lionel Groulx'
 ]
 
 def generate_event():
@@ -41,9 +34,6 @@ def generate_event():
     return ret
 
 
-def generate_event_atm():
-    ret = ATM_EVENT[0]
-    return ret
 
 
 def main(args):
@@ -58,7 +48,6 @@ def main(args):
     while True:
         logging.info(json.dumps(generate_event()).encode())
         producer.send(args.topic, json.dumps(generate_event()).encode(), json.dumps(CUSTOMER[random.randint(0, 1)]).encode())
-        producer.send("ATM_Withdrawal", json.dumps(generate_event_atm()).encode(), json.dumps(CUSTOMER[0]).encode())
         time.sleep(10.0)
     logging.info('end sending events')
 
